f61dc5a981ffa5c978e8c1c95034bf20384bd551
Mateusz <mateusz.ostro@o2.pl>
2023-10-17 22:25:40 +0200

    injected repository and controller

--- src/main/java/pl/expiredateapp/services/ProductService.java
+++ src/main/java/pl/expiredateapp/services/ProductService.java
@@ -1,7 +1,12 @@
 package pl.expiredateapp.services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.expiredateapp.repositories.ProductRepository;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private ProductRepository productRepository;
 }
