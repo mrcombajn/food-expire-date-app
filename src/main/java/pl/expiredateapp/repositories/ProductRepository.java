cc3dd69fdf2ecc5cb566592579a0b94d74daed27
Mateusz <mateusz.ostro@o2.pl>
2023-10-17 22:21:56 +0200

    Created controller layer

---
+++ src/main/java/pl/expiredateapp/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package pl.expiredateapp.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+}
---
+++ src/main/java/pl/expiredateapp/services/ProductService.java
@@ -0,0 +1,7 @@
+package pl.expiredateapp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+}
