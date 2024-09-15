cd4cd93492f01399d7f98471021e88137a899819
Mateusz <mateusz.ostro@o2.pl>
2024-09-15 14:01:26 +0200

    Implemented exception handler

--- src/main/java/pl/expiredateapp/controllers/handlers/ProductControllerExceptionHandler.java
+++ src/main/java/pl/expiredateapp/controllers/handlers/ProductControllerExceptionHandler.java
@@ -1,8 +1,21 @@
 package pl.expiredateapp.controllers.handlers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import pl.expiredateapp.entities.exceptions.EntityNotFoundException;
+
+import java.util.Map;
+
+@ControllerAdvice
 public class ProductControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleMissingDatabaseProduct(EntityNotFoundException e) {
+        return new ResponseEntity<>(Map.of("error", e.getMessage()) , HttpStatus.BAD_REQUEST);
+    }
 
 }
