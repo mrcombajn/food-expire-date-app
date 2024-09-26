c8c811972b16b39b351d58ff8e9cddfc69dd99e1
Mateusz <mateusz.ostro@o2.pl>
2024-09-27 00:22:20 +0200

    last changes

--- src/main/java/pl/expiredateapp/FoodExpireDateApplication.java
+++ src/main/java/pl/expiredateapp/FoodExpireDateApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings({"PMD", "checkstyle:hideutilityclassconstructor"})
 public class FoodExpireDateApplication {
 
     /**
